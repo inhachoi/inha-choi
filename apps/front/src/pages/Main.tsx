8ec099837aa9436cd12885cb8f5dd4b63e7cf93a
inhachoi <chlruddlf73@naver.com>
2025-11-27 21:02:45 +0900

    chore: change text

--- apps/front/src/pages/Main.tsx
+++ apps/front/src/pages/Main.tsx
@@ -2,7 +2,7 @@ export function Main() {
   return (
     <>
       {Array.from({ length: 100 }).map((_, index) => (
-        <div key={index}>Main 화면 개발 중...</div>
+        <div key={index}>언주 바보 ㅋㅋ</div>
       ))}
     </>
   );
