75f4a2806771173203cf02f482bcc754a4f0e0c6
inhachoi <chlruddlf73@naver.com>
2025-11-29 15:12:27 +0900

    feat: add url constant manage

---
+++ apps/front/src/constants/index.ts
@@ -0,0 +1,5 @@
+export const MY_VELOG_URL = "https://velog.io/@chlruddlf73/posts";
+export const MY_GITHUB_URL = "https://github.com/inhachoi";
+export const PR_REACT_JS_CUSTOM_HOOK_URL = "https://github.com/reactjs/ko.react.dev/pull/1355"
+export const PR_REACT_JS_EFFECT_URL = "https://github.com/reactjs/ko.react.dev/pull/1356#issuecomment-3567598733"
+export const PR_MDN_ASYNC_URL = "https://github.com/mdn/translated-content/pull/28427"
