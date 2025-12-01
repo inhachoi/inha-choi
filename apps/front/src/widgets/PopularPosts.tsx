1c445760d95f9ec3da899141fa6c8308ea077a57
inhachoi <chlruddlf73@naver.com>
2025-12-01 21:43:28 +0900

    feat: add date in Article component

--- apps/front/src/widgets/PopularPosts.tsx
+++ apps/front/src/widgets/PopularPosts.tsx
@@ -5,9 +5,17 @@ import { usePopularPosts } from "../hooks";
 export function PopularPosts() {
   const popularPosts = usePopularPosts();
 
+  const formatYearMonth = (iso: string) => {
+    const date = new Date(iso);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    return `${year}.${month}`;
+  };
+
   return (
     <Container>
       <Title>Popular Posts</Title>
+
       {popularPosts.map((post) => (
         <Article
           key={post.link}
@@ -15,6 +23,7 @@ export function PopularPosts() {
           link={post.link}
           thumbnail={post.thumbnail}
           likes={post.likes}
+          released_at={formatYearMonth(post.released_at)}
         />
       ))}
     </Container>
