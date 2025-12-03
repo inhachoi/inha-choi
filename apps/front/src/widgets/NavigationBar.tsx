50530359f8752f6913dcc4245cefa23bf7a9168a
inhachoi <chlruddlf73@naver.com>
2025-12-03 10:38:48 +0900

    chore: Hide incomplete pages

--- apps/front/src/widgets/NavigationBar.tsx
+++ apps/front/src/widgets/NavigationBar.tsx
@@ -9,8 +9,8 @@ export function NavigationBar() {
         <LogoButton />
         <TextButtonGroup>
           <TextButton toGo="/posts">Posts</TextButton>
-          <TextButton toGo="/study">Study</TextButton>
-          <TextButton toGo="/visitor">Visitor</TextButton>
+          {/* <TextButton toGo="/study">Study</TextButton>
+          <TextButton toGo="/visitor">Visitor</TextButton> */}
         </TextButtonGroup>
       </Wrapper>
     </Container>
