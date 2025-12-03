1e8404791401dc84e066a262795c3e984a304ca1
inhachoi <chlruddlf73@naver.com>
2025-12-03 10:58:19 +0900

    feat: basic Posts page setting

--- apps/front/src/pages/Posts.tsx
+++ apps/front/src/pages/Posts.tsx
@@ -1,5 +1,43 @@
+import { usePosts } from "../hooks";
+import styled from "@emotion/styled";
+import { Article } from "../components";
+import { formatYearMonth } from "../utils";
+
 export function Posts() {
-  return <div>Posts 화면 개발 중...</div>;
+  const posts = usePosts();
+
+  return (
+    <Container>
+      <PostsHeader>{posts.length} posts</PostsHeader>
+      <AllPosts>
+        {posts.map((post) => (
+          <Article
+            key={post.link}
+            title={post.title}
+            link={post.link}
+            thumbnail={post.thumbnail}
+            likes={post.likes}
+            released_at={formatYearMonth(post.released_at)}
+          />
+        ))}
+      </AllPosts>
+    </Container>
+  );
 }
-  
-  
+
+const Container = styled.div``;
+
+const AllPosts = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin: 50px 0;
+`;
+
+const PostsHeader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  margin: 50px 0;
+`;
