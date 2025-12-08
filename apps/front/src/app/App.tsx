5f96945085be7e03d799613e18de255ddfb8d8e9
inhachoi <chlruddlf73@naver.com>
2025-12-08 11:26:22 +0900

    feat: hide scrollbar with GlobalStyles

--- apps/front/src/app/App.tsx
+++ apps/front/src/app/App.tsx
@@ -1,22 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainPage, PostsPage, StudyPage, GuestbookPage } from "@/pages";
-import { NavigationBar } from "./widgets/NavigationBar";
+import { NavigationBar, Footer } from "@/widgets";
 import styled from "@emotion/styled";
 import { colors } from "@toss/tds-colors";
-import { Footer } from "@/widgets";
+import { GlobalStyles } from "./GlobalStyles";
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <NavigationBar />
-      <Container>
+      <PageLayout>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/posts" element={<PostsPage />} />
           <Route path="/study" element={<StudyPage />} />
           <Route path="/guestbook" element={<GuestbookPage />} />
         </Routes>
-      </Container>
+      </PageLayout>
       <Footer />
     </BrowserRouter>
   );
@@ -24,11 +25,19 @@ function App() {
 
 export default App;
 
-const Container = styled.div`
+const PageLayout = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   background: ${colors.grey50};
-  margin: 60px auto 0 auto;
+  margin: 60px 0 0 0;
   min-width: 370px;
+
+  @media (max-width: 768px) {
+    margin: 51px 0 0 0;
+  }
+
+  @media (max-width: 480px) {
+    margin: 42px 0 0 0;
+  }
 `;
