28c497c008e70da64c398af0209887b1b96f0f98
inhachoi <chlruddlf73@naver.com>
2025-12-27 01:08:33 +0900

    feat: add ErrorBoundeary

--- apps/front/src/app/main.tsx
+++ apps/front/src/app/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/app/App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ErrorBoundary fallback={<div>에러임~~~~</div>}>
+        <App />
+      </ErrorBoundary>
     </QueryClientProvider>
   </StrictMode>
 );
