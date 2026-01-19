7d6f7103ddd5dcd940ccb08534eb043a17af86e7
inhachoi <chlruddlf73@naver.com>
2026-01-19 18:06:10 +0900

    fix: Correct incorrectly named props name to ~DTO

--- apps/front/src/pages/guestbook-page/model/types.ts
+++ apps/front/src/pages/guestbook-page/model/types.ts
@@ -5,15 +5,6 @@ export interface UserDTO {
   profileUrl?: string;
 }
 
-export interface CommentWriterDTO {
-  user: UserDTO;
-  content: string;
-  setContent: React.Dispatch<React.SetStateAction<string>>;
-  submitting: boolean;
-  handleLogin: () => void;
-  handleSubmit: (e: React.FormEvent) => Promise<void>;
-}
-
 export interface CommentDTO {
   id: string;
   content: string;
