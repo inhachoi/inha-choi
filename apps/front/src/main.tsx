8223698db0650e2b0de0ff4919fa468ea70a7f38
inhachoi <chlruddlf73@naver.com>
2026-03-19 12:28:52 +0900

    feat: Sentry 모니터링 추가

--- apps/front/src/main.tsx
+++ apps/front/src/main.tsx
@@ -1,15 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 
 import App from "@/app/App.tsx";
 import { AppProviders } from "@/app/providers/AppProviders";
 
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 
+Sentry.init({
+  dsn: "https://d7cc6f47f62bf2a243307eadaca0f048@o4511053851066368.ingest.us.sentry.io/4511069006266368",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppProviders>
       <App />
     </AppProviders>
-  </StrictMode>
+  </StrictMode>,
 );
